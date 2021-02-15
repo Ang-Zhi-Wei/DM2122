2197b97f43d6cd48903805e0f79cdf10267d2ac4
Jiawei <200534U@mymail.nyp.edu.sg>
2021-02-15 17:04:47 +0800

    Half done Room1 (Application is currently for Assignment2Room1, so switch it to whichever scene you're doing, pushing to fix code)

---
+++ Application/Source/SceneSP2Room1.h
@@ -0,0 +1,138 @@
+#ifndef SCENESceneSP2Room1_H
+#define SCENESceneSP2Room1_H
+#include "Scene.h"
+#include "Mtx44.h"
+#include "Application.h"
+#include "Camera4.h"
+#include "CameraSP2.h"
+#include "Mesh.h"
+#include "MeshBuilder.h"
+#include "MatrixStack.h"
+#include "Light.h"
+#include "Utility.h"
+#include "ColliderBox.h"
+class SceneSP2Room1 : public Scene
+{
+public:
+	SceneSP2Room1();
+	~SceneSP2Room1();
+
+	virtual void Init();
+	virtual void Update(double dt);
+	virtual void Render();
+	virtual void Exit();
+	enum GEOMETRY_TYPE
+	{
+		GEO_AXES,
+		LightSphere,
+
+		//skybox
+		GEO_LEFT,
+		GEO_RIGHT,
+		GEO_TOP,
+		GEO_BOTTOM,
+		GEO_FRONT,
+		GEO_BACK,
+
+		//ground mesh
+		Ground_Mesh,
+
+		//objs
+		GEO_BUILDING,
+		Ruins,
+		Colliderbox,
+
+		//UI tings
+		GEO_TEXT,
+		GEO_OVERLAY,
+
+		
+		NUM_GEOMETRY,
+	};
+	enum UNIFORM_TYPE
+	{
+		U_MVP = 0,
+		U_MODELVIEW,
+		U_MODELVIEW_INVERSE_TRANSPOSE,
+		U_MATERIAL_AMBIENT,
+		U_MATERIAL_DIFFUSE,
+		U_MATERIAL_SPECULAR,
+		U_MATERIAL_SHININESS,
+		U_NUMLIGHTS,
+		U_COLOR_TEXTURE_ENABLED,
+		U_COLOR_TEXTURE,
+		U_TEXT_ENABLED,
+		U_TEXT_COLOR,
+		//light 0
+		U_LIGHT0_POSITION,
+		U_LIGHT0_COLOR,
+		U_LIGHT0_POWER,
+		U_LIGHT0_KC,
+		U_LIGHT0_KL,
+		U_LIGHT0_KQ,
+		U_LIGHTENABLED,
+		U_LIGHT0_TYPE,
+		U_LIGHT0_SPOTDIRECTION,
+		U_LIGHT0_COSCUTOFF,
+		U_LIGHT0_COSINNER,
+		U_LIGHT0_EXPONENT,
+		//light 1
+		U_LIGHT1_POSITION,
+		U_LIGHT1_COLOR,
+		U_LIGHT1_POWER,
+		U_LIGHT1_KC,
+		U_LIGHT1_KL,
+		U_LIGHT1_KQ,
+		U_LIGHT1_TYPE,
+		U_LIGHT1_SPOTDIRECTION,
+		U_LIGHT1_COSCUTOFF,
+		U_LIGHT1_COSINNER,
+		U_LIGHT1_EXPONENT,
+		
+		U_TOTAL,
+
+	};
+
+private:
+
+	struct Item
+	{
+
+	};
+
+	struct Inventory
+	{
+		Item* selected;
+		Item* inventory[10];
+	};
+
+	void RenderSkybox();
+	//void RenderMegumin();
+	//void RenderDeadTree(int x, int z,float rotate);
+	unsigned m_vertexArrayID;
+	unsigned m_programID;
+	unsigned m_parameters[U_TOTAL];
+	Mesh* meshList[NUM_GEOMETRY];
+	
+	MS modelStack, viewStack, projectionStack;
+	Light light[2];
+	CameraSP2 camera;
+	float LSPEED;
+	float fps;
+
+
+
+	bool flashlight;
+	bool Fpressed, Freleased;
+	bool Epressed, Ereleased;
+	bool Qpressed, Qreleased;
+
+	ColliderBox Ruincollider;
+
+	void RenderMesh(Mesh* mesh, bool enableLight);
+	void RenderText(Mesh* mesh, std::string text, Color color);
+	void RenderTextOnScreen(Mesh* mesh, std::string text, Color color, float size, float x, float y);
+	void RenderMeshOnScreen(Mesh* mesh, int x, int y, int sizex, int sizey);
+};
+
+#endif
